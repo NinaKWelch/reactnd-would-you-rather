3d141ebb7a22487c47bdb8c450fb1978a15f21f4
NinaWelch <nwelch1@live.co.uk>
2020-07-14 15:32:44 +0100

    docs: add description

--- src/components/App.js
+++ src/components/App.js
@@ -16,6 +16,7 @@ const App = (props) => {
   // get data
   useEffect(() => {
     props.handleInitialData();
+  // eslint-disable-next-line
   }, []);
 
   const { authedUser, questionIds, users } = props;
