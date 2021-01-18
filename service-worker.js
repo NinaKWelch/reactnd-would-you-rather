6375fef8431bfe1841bd2c49ad40ce43fdf270c7
NinaWelch <nwelch1@live.co.uk>
2021-01-18 10:00:19 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactnd-would-you-rather/precache-manifest.aad1cd4bd8f5a18cf973fc19c9c7ffb2.js"
+  "/reactnd-would-you-rather/precache-manifest.6b75e5b4087516aa806c153fa355ee49.js"
 );
 
 self.addEventListener('message', (event) => {
